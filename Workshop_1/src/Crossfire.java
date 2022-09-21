a2a7e9919de83b4ee194931938ad28e78c543710
Kristiyan Stoyanov <kristiyanstoyanov@Kristiyans-MacBook-Air.local>
2022-09-21 18:14:44 +0300

    fixed typo in class def

--- Workshop_1/src/Crossfire.java
+++ Workshop_1/src/Crossfire.java
@@ -2,7 +2,7 @@ import java.util.Arrays;
 import java.util.List;
 import java.util.Scanner;
 
-public class Main {
+public class Crossfire {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         int[] dimensions = Arrays.stream(scanner.nextLine().split(" "))
